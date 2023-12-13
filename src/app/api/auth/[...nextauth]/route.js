76f6c297d98343af5697ffe55c69976092c2f937
naiemhosen430 <naiemhosen430@gmail.com>
2023-12-13 00:20:43 -0800

    d

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,13 +1,11 @@
-// pages/api/auth/[...nextauth].js
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 const hundler = NextAuth({
   providers: [
     GoogleProvider({
-      clientId:
-        "796637101114-55uoocqpg4mi3pe1at1rj46n8br1hide.apps.googleusercontent.com",
-      clientSecret: "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",
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
 });
