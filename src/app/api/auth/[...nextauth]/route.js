66c039202d834d2bf95938862a8972884068d3d1
naiemhosen430 <naiemhosen430@gmail.com>
2023-12-13 18:52:36 -0800

    d

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -7,8 +7,6 @@ const hundler = NextAuth({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       redirectUri: "https://tmanaiem.vercel.app/auth/callback/google",
-      authorizationUrl: "https://accounts.google.com/o/oauth2/auth",
-      requestTokenUrl: "https://accounts.google.com/o/oauth2/token",
     }),
   ],
 });
