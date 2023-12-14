8daabc37af741a788af5a6d474d6689735d18c39
naiemhosen430 <naiemhosen430@gmail.com>
2023-12-13 18:49:12 -0800

    d

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -4,8 +4,9 @@ import GoogleProvider from "next-auth/providers/google";
 const hundler = NextAuth({
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId:
+        "7101114-55uoocqpg4mi3pe1at1rj46n8br1hide.apps.googleusercontent.com",
+      clientSecret: "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",
       redirectUri: "https://tmanaiem.vercel.app/auth/callback/google",
       authorizationUrl: "https://accounts.google.com/o/oauth2/auth",
       requestTokenUrl: "https://accounts.google.com/o/oauth2/token",
